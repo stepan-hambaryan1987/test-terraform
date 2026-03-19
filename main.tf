7ac3fe0eac0f0ca240270e7310e81339c50706b2
stepan hambaryan <stepan-hambaryan1987@mail.ru>
2026-03-19 13:47:14 +0400

    added backend

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-styop-1987"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
